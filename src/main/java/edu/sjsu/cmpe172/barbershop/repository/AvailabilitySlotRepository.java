481fab538b0d13a96571a0ae830158783326bede
Thai Nguyen <nguyenhoathai.nht@gmail.com>
2026-03-07 22:09:37 -0800

    Create mockup data for availabilitySlot

---
+++ src/main/java/edu/sjsu/cmpe172/barbershop/repository/AvailabilitySlotRepository.java
@@ -0,0 +1,28 @@
+package edu.sjsu.cmpe172.barbershop.repository;
+
+import org.springframework.stereotype.Repository;
+import edu.sjsu.cmpe172.barbershop.model.AvailabilitySlot;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class AvailabilitySlotRepository {
+    public List<AvailabilitySlot> findAvailabilitySlots(Long providerId, String date) {
+        List<AvailabilitySlot> slots = new ArrayList<>();
+        LocalDate selectedDate = LocalDate.parse(date);
+
+        //Mockup data
+        slots.add(new AvailabilitySlot(1L, providerId, selectedDate, LocalTime.of(9, 0), true));
+        slots.add(new AvailabilitySlot(2L, providerId, selectedDate, LocalTime.of(10, 0), true));
+        slots.add(new AvailabilitySlot(3L, providerId, selectedDate, LocalTime.of(11, 0), true));
+        slots.add(new AvailabilitySlot(4L, providerId, selectedDate, LocalTime.of(15, 0), true));
+        slots.add(new AvailabilitySlot(5L, providerId, selectedDate, LocalTime.of(16, 30), true));
+        slots.add(new AvailabilitySlot(6L, providerId, selectedDate, LocalTime.of(18, 0), true));
+        
+        return slots;
+    }
+    
+}
