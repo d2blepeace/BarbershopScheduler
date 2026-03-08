3e0f457d8ebd39453dda511c7146cb7328e4b5f7
Thai Nguyen <nguyenhoathai.nht@gmail.com>
2026-03-07 22:31:15 -0800

    Working endpoint for Appointment in controller

---
+++ src/main/java/edu/sjsu/cmpe172/barbershop/controller/AvailabilityController.java
@@ -0,0 +1,24 @@
+package edu.sjsu.cmpe172.barbershop.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.sjsu.cmpe172.barbershop.model.AvailabilitySlot;
+import edu.sjsu.cmpe172.barbershop.service.SalonService;
+
+public class AvailabilityController {
+    private final SalonService salonService;
+
+    public AvailabilityController(SalonService salonService) {
+        this.salonService = salonService;
+    }
+
+    @GetMapping("/slots")
+    public List<AvailabilitySlot> geAvailabilitySlots(
+            @RequestParam Long providerId, 
+            @RequestParam String date) {
+        return salonService.geAvailabilitySlots(providerId, date);
+    }
+}
