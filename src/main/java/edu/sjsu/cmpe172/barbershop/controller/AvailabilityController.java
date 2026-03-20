e1d59bd1f76d803378bedcecb951a3961664b62d
Thai Nguyen <nguyenhoathai.nht@gmail.com>
2026-03-20 15:29:55 -0700

    Adjust controller comments

--- src/main/java/edu/sjsu/cmpe172/barbershop/controller/AvailabilityController.java
+++ src/main/java/edu/sjsu/cmpe172/barbershop/controller/AvailabilityController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.sjsu.cmpe172.barbershop.model.AvailabilitySlot;
 import edu.sjsu.cmpe172.barbershop.service.SalonService;
 
+/**
+ * This ccontroller handles request related to available time slots
+ * Customer can query available times for a specific provider on a given date
+ * NOTES: need refining to work better
+ */
 @RestController
 public class AvailabilityController {
     private final SalonService salonService;
@@ -17,6 +22,12 @@ public class AvailabilityController {
         this.salonService = salonService;
     }
 
+    /**
+     * Return the available appointment slots for a specific provider
+     * @param providerId - barber/technician ID
+     * @param date       - date to check for availability (YYYY-MM-DD)
+     * @return available slots
+     */
     @GetMapping("/slots")
     public List<AvailabilitySlot> getAvailabilitySlots(
             @RequestParam Long providerId, 
--- src/main/java/edu/sjsu/cmpe172/barbershop/controller/ServiceController.java
+++ src/main/java/edu/sjsu/cmpe172/barbershop/controller/ServiceController.java
@@ -8,13 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * This controller handles HTTP request relate to salon services
+ * Controller will communicate with Service Layer (SalonService),
+ * then retrieves data from Repo Layer
+ * Client Request -> Controller -> SalonService -> ServiceRepo -> MockingData
+ */
 @RestController
 public class ServiceController {
     private final SalonService salonService;
+    
+    //Constructor 
     public ServiceController(SalonService salonService) {
         this.salonService = salonService;
     }
 
+    // Return list of all services available of the system
     @GetMapping("/services") 
     public List<Service> getServices() {
         return salonService.getAllServices();
