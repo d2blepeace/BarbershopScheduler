bdfe7e6859f8fb5d2298c5965ceb1709f27352ff
Thai Nguyen <nguyenhoathai.nht@gmail.com>
2026-03-21 21:57:04 -0700

    Remove exclude database to test

--- src/main/java/edu/sjsu/cmpe172/barbershop/BarbershopApplication.java
+++ src/main/java/edu/sjsu/cmpe172/barbershop/BarbershopApplication.java
@@ -2,11 +2,7 @@ package edu.sjsu.cmpe172.barbershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-
-
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class BarbershopApplication {
 
 	public static void main(String[] args) {
